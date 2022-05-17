d9ebbacefbff9e19ccfc02e8ba6dc65315df100b
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-05-17 15:37:47 -0700

    commit tree e4e9515c9a1010d3839dbf9526418e544049c266

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
+
+function distanceFromHqInFeet(sumValue) {
+  return distanceFromHqInBlocks(sumValue) * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start < destination) {
+    return (destination - start) * 264;
+  } else {
+    return (start - destination) * 264;
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  const calculate = distanceTravelledInFeet(start, destination);
+  if (calculate <= 400) {
+    return 0;
+  } else if (calculate > 400 && calculate <= 2000) {
+    return 0.02 * calculate;
+    return 2.56;
+  } else if (calculate > 2000 && calculate < 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
